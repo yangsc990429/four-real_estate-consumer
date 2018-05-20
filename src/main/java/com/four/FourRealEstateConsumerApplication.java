c0a0f3064ec81ab2fad209a69d37305156faabef
yangsc990429 <319653885@qq.com>
2018-05-20 20:50:39 +0800

    four-real_estate-consumer四组房地产消费者

---
+++ src/main/java/com/four/FourRealEstateConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+/*@ImportResource(locations="dubbo-consumer.xml")*/
+public class FourRealEstateConsumerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FourRealEstateConsumerApplication.class, args);
+	}
+}
