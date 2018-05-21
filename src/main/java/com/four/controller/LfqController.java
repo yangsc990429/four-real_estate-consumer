f3528b2b0c0d4c4dc448525deca7451970bd0f80
liufengqi <41436911@qq.com>
2018-05-21 17:14:22 +0800

    登录存session

--- src/main/java/com/four/controller/LfqController.java
+++ src/main/java/com/four/controller/LfqController.java
@@ -5,10 +5,12 @@ import com.four.entity.Tree;
 import com.four.entity.User;
 import com.four.service.LfqService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -21,11 +23,19 @@ public class LfqController {
     //登陆
     @RequestMapping("queryuser")
     @ResponseBody
-    public String queryuser(User user){
+    public String queryuser(User user, HttpServletRequest request){
         System.err.println("aaaaaaaaaaa"+user);
         String usjsp = lfqService.queryuser(user);
-        System.err.println(usjsp);
-        return usjsp;
+        if (Integer.valueOf(usjsp.substring(0,1))==1){
+            user.setUserid(Integer.valueOf(usjsp.substring(1)));
+            request.getSession().setAttribute("user",user);
+            Object user1 = request.getSession().getAttribute("user");
+            System.err.println(user1+"7777777777777");
+            return 1+"";
+        }else{
+            return usjsp;
+        }
+
     }
 
     //查询树
