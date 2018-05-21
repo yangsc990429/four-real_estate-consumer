7f88c56783db9090aaf2188127847834de94338f
zhangxinghua <zxh1511704850@outlook.com>
2018-05-21 11:05:48 +0800

    改类

--- src/main/java/com/four/controller/ZxhController.java
+++ src/main/java/com/four/controller/ZxhController.java
@@ -1,5 +1,15 @@
 package com.four.controller;
 
+import com.four.service.ZxhService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("zxh")
 public class ZxhController {
+
+    @Autowired
+    private ZxhService zxhservice;
 
 }
