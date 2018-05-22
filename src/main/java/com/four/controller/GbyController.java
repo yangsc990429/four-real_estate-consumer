00b4beae2c8c38e098e7bde19e3c88cfbe7eff6c
guobaoyu <1751845992@qq.com>
2018-05-22 19:35:21 +0800

    上传图片

---
+++ src/main/java/com/four/controller/GbyController.java
@@ -0,0 +1,71 @@
+package com.four.controller;
+
+import com.four.entity.Apartment;
+import com.four.service.GbyService;
+import com.four.util.AliyunOSSClientUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("gby")
+public class GbyController {
+    @Autowired
+    private GbyService gbyService;
+
+    @RequestMapping("wuyeleixing")
+    public String wuyeleixing(){
+        return "gby/wuyeleixing";
+    }
+    //查询物业类型
+    @RequestMapping("queryApartment")
+    @ResponseBody
+    public List<Apartment> queryApartment() {
+        List<Apartment> list=gbyService.queryApartment();
+        return list;
+    }
+    //排序
+    @RequestMapping("querycount")
+    @ResponseBody
+    public  Integer querycount(){
+      Integer paixu=gbyService.querycount();
+      return  paixu;
+    }
+    //添加
+    @RequestMapping("addApartment")
+    @ResponseBody
+    public  void addApartment(Apartment apartment){
+        gbyService.addApartment(apartment);
+    }
+    //添加图片
+    @RequestMapping(value = "/addPicture", method = RequestMethod.POST, produces = "application/json;charset=utf8")
+    @ResponseBody
+    public  Object addPicture(@RequestParam("file") MultipartFile[] files){
+        String greatBeauty = AliyunOSSClientUtil.GreatBeauty(files[0], files[0].getOriginalFilename());
+        Map map = new HashMap(1);
+        map.put("a",greatBeauty);
+        return map;
+    }
+    //删除
+    @RequestMapping("deleteApartment")
+    @ResponseBody
+    public void deleteApartment(Integer id){
+        gbyService.deleteApartment(id);
+    }
+    //回显
+    @RequestMapping("queryById")
+    @ResponseBody
+    public  Apartment queryById(Integer id){
+        return  gbyService.queryById(id);
+    }
+
+
+}
---
+++ src/main/java/com/four/service/GbyService.java
@@ -0,0 +1,20 @@
+package com.four.service;
+
+import com.four.entity.Apartment;
+import com.four.entity.Area;
+import com.four.entity.Business;
+
+import java.util.List;
+
+public interface GbyService {
+
+    List<Apartment> queryApartment();
+
+    Integer querycount();
+
+    void addApartment(Apartment apartment);
+
+    void deleteApartment(Integer id);
+
+    Apartment queryById(Integer id);
+}
