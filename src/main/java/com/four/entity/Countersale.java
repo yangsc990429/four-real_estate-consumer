51d9c5bcbab2169abaf15be348504687460a1e53
zhangxinghua <zxh1511704850@outlook.com>
2018-05-24 14:03:12 +0800

    石铁类

---
+++ src/main/java/com/four/entity/Countersale.java
@@ -0,0 +1,400 @@
+package com.four.entity;
+
+public class Countersale {
+    private Integer id;
+    private String name;
+    private Integer leid;
+    private String biancount;
+    private String acreage;
+    private String price;
+    private String xiaoqu;
+    private String louhao;
+    private String danyuan;
+    private String shi;
+    private Integer loudi;
+    private Integer lougon;
+    private Integer areaid;
+    private Integer printid;
+    private String weizhi;
+    private String chuphoto;
+    private String jiaoid;
+    private String diid;
+    private Integer schoolid;
+    private String xueid;
+    private String huxin;
+    private String wuprice;
+    private Integer kuangid;
+    private Integer chaoid;
+    private Integer dateid;
+    private String chan;
+    private String jinma;
+    private String pinzhi;
+    private String vrlian;
+    private String miaodiscir;
+    private Integer shangjia;
+    private Integer chuchuang;
+    private String lianren;
+    private String lianphone;
+    private Integer fabuid;
+    private Integer xuhao;
+    private Integer zhixuhao;
+    private String createdate;
+    private Integer shenheid;
+
+    @Override
+    public String toString() {
+        return "Countersale{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", leid=" + leid +
+                ", biancount='" + biancount + '\'' +
+                ", acreage='" + acreage + '\'' +
+                ", price='" + price + '\'' +
+                ", xiaoqu='" + xiaoqu + '\'' +
+                ", louhao='" + louhao + '\'' +
+                ", danyuan='" + danyuan + '\'' +
+                ", shi='" + shi + '\'' +
+                ", loudi=" + loudi +
+                ", lougon=" + lougon +
+                ", areaid=" + areaid +
+                ", printid=" + printid +
+                ", weizhi='" + weizhi + '\'' +
+                ", chuphoto='" + chuphoto + '\'' +
+                ", jiaoid='" + jiaoid + '\'' +
+                ", diid='" + diid + '\'' +
+                ", schoolid=" + schoolid +
+                ", xueid='" + xueid + '\'' +
+                ", huxin='" + huxin + '\'' +
+                ", wuprice='" + wuprice + '\'' +
+                ", kuangid=" + kuangid +
+                ", chaoid=" + chaoid +
+                ", dateid=" + dateid +
+                ", chan='" + chan + '\'' +
+                ", jinma='" + jinma + '\'' +
+                ", pinzhi='" + pinzhi + '\'' +
+                ", vrlian='" + vrlian + '\'' +
+                ", miaodiscir='" + miaodiscir + '\'' +
+                ", shangjia=" + shangjia +
+                ", chuchuang=" + chuchuang +
+                ", lianren='" + lianren + '\'' +
+                ", lianphone='" + lianphone + '\'' +
+                ", fabuid=" + fabuid +
+                ", xuhao=" + xuhao +
+                ", zhixuhao=" + zhixuhao +
+                ", createdate='" + createdate + '\'' +
+                ", shenheid=" + shenheid +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getLeid() {
+        return leid;
+    }
+
+    public void setLeid(Integer leid) {
+        this.leid = leid;
+    }
+
+    public String getBiancount() {
+        return biancount;
+    }
+
+    public void setBiancount(String biancount) {
+        this.biancount = biancount;
+    }
+
+    public String getAcreage() {
+        return acreage;
+    }
+
+    public void setAcreage(String acreage) {
+        this.acreage = acreage;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getXiaoqu() {
+        return xiaoqu;
+    }
+
+    public void setXiaoqu(String xiaoqu) {
+        this.xiaoqu = xiaoqu;
+    }
+
+    public String getLouhao() {
+        return louhao;
+    }
+
+    public void setLouhao(String louhao) {
+        this.louhao = louhao;
+    }
+
+    public String getDanyuan() {
+        return danyuan;
+    }
+
+    public void setDanyuan(String danyuan) {
+        this.danyuan = danyuan;
+    }
+
+    public String getShi() {
+        return shi;
+    }
+
+    public void setShi(String shi) {
+        this.shi = shi;
+    }
+
+    public Integer getLoudi() {
+        return loudi;
+    }
+
+    public void setLoudi(Integer loudi) {
+        this.loudi = loudi;
+    }
+
+    public Integer getLougon() {
+        return lougon;
+    }
+
+    public void setLougon(Integer lougon) {
+        this.lougon = lougon;
+    }
+
+    public Integer getAreaid() {
+        return areaid;
+    }
+
+    public void setAreaid(Integer areaid) {
+        this.areaid = areaid;
+    }
+
+    public Integer getPrintid() {
+        return printid;
+    }
+
+    public void setPrintid(Integer printid) {
+        this.printid = printid;
+    }
+
+    public String getWeizhi() {
+        return weizhi;
+    }
+
+    public void setWeizhi(String weizhi) {
+        this.weizhi = weizhi;
+    }
+
+    public String getChuphoto() {
+        return chuphoto;
+    }
+
+    public void setChuphoto(String chuphoto) {
+        this.chuphoto = chuphoto;
+    }
+
+    public String getJiaoid() {
+        return jiaoid;
+    }
+
+    public void setJiaoid(String jiaoid) {
+        this.jiaoid = jiaoid;
+    }
+
+    public String getDiid() {
+        return diid;
+    }
+
+    public void setDiid(String diid) {
+        this.diid = diid;
+    }
+
+    public Integer getSchoolid() {
+        return schoolid;
+    }
+
+    public void setSchoolid(Integer schoolid) {
+        this.schoolid = schoolid;
+    }
+
+    public String getXueid() {
+        return xueid;
+    }
+
+    public void setXueid(String xueid) {
+        this.xueid = xueid;
+    }
+
+    public String getHuxin() {
+        return huxin;
+    }
+
+    public void setHuxin(String huxin) {
+        this.huxin = huxin;
+    }
+
+    public String getWuprice() {
+        return wuprice;
+    }
+
+    public void setWuprice(String wuprice) {
+        this.wuprice = wuprice;
+    }
+
+    public Integer getKuangid() {
+        return kuangid;
+    }
+
+    public void setKuangid(Integer kuangid) {
+        this.kuangid = kuangid;
+    }
+
+    public Integer getChaoid() {
+        return chaoid;
+    }
+
+    public void setChaoid(Integer chaoid) {
+        this.chaoid = chaoid;
+    }
+
+    public Integer getDateid() {
+        return dateid;
+    }
+
+    public void setDateid(Integer dateid) {
+        this.dateid = dateid;
+    }
+
+    public String getChan() {
+        return chan;
+    }
+
+    public void setChan(String chan) {
+        this.chan = chan;
+    }
+
+    public String getJinma() {
+        return jinma;
+    }
+
+    public void setJinma(String jinma) {
+        this.jinma = jinma;
+    }
+
+    public String getPinzhi() {
+        return pinzhi;
+    }
+
+    public void setPinzhi(String pinzhi) {
+        this.pinzhi = pinzhi;
+    }
+
+    public String getVrlian() {
+        return vrlian;
+    }
+
+    public void setVrlian(String vrlian) {
+        this.vrlian = vrlian;
+    }
+
+    public String getMiaodiscir() {
+        return miaodiscir;
+    }
+
+    public void setMiaodiscir(String miaodiscir) {
+        this.miaodiscir = miaodiscir;
+    }
+
+    public Integer getShangjia() {
+        return shangjia;
+    }
+
+    public void setShangjia(Integer shangjia) {
+        this.shangjia = shangjia;
+    }
+
+    public Integer getChuchuang() {
+        return chuchuang;
+    }
+
+    public void setChuchuang(Integer chuchuang) {
+        this.chuchuang = chuchuang;
+    }
+
+    public String getLianren() {
+        return lianren;
+    }
+
+    public void setLianren(String lianren) {
+        this.lianren = lianren;
+    }
+
+    public String getLianphone() {
+        return lianphone;
+    }
+
+    public void setLianphone(String lianphone) {
+        this.lianphone = lianphone;
+    }
+
+    public Integer getFabuid() {
+        return fabuid;
+    }
+
+    public void setFabuid(Integer fabuid) {
+        this.fabuid = fabuid;
+    }
+
+    public Integer getXuhao() {
+        return xuhao;
+    }
+
+    public void setXuhao(Integer xuhao) {
+        this.xuhao = xuhao;
+    }
+
+    public Integer getZhixuhao() {
+        return zhixuhao;
+    }
+
+    public void setZhixuhao(Integer zhixuhao) {
+        this.zhixuhao = zhixuhao;
+    }
+
+    public String getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(String createdate) {
+        this.createdate = createdate;
+    }
+
+    public Integer getShenheid() {
+        return shenheid;
+    }
+
+    public void setShenheid(Integer shenheid) {
+        this.shenheid = shenheid;
+    }
+}
